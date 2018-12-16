27cf8f90691ef14273bdf6063aeee0c4f1c303ae
John Spink <john.spink@gmail.com>
2018-12-16 15:59:06 +0000

    Lecture 12-139 - Challenge There is a bug in the location constructor. Find and fix it.

--- src/Location.java
+++ src/Location.java
@@ -3,6 +3,16 @@
 // Class added by John Spink on 16/12/2018
 // ---------------------------------------------------------
 
+// Challenge Lecture 12-139
+//
+// The challenge here is to work out what is wrong with this location constructor
+// that would allow the program to compile and crash at runtime.
+// When you identify the problem, modify the code to fix it.
+// I’ll just say that again your challenge here is to work out what is wrong with
+// the location constructor that you can see on screen line 14 that will allow the
+// program to compile but crash on run time when you identify the problem modify
+// the code to fix it.
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,13 +24,13 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>(exits);
+        if(exits != null) {
+            this.exits = new HashMap<String, Integer>(exits);
+        } else {
+            this.exits = new HashMap<String, Integer>();
+        }
         this.exits.put("Q", 0);
     }
-
-//    public void addExit(String direction, int location) {
-//        exits.put(direction, location);
-//    }
 
     public int getLocationID() {
         return locationID;
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,7 @@
 // Class added by John Spink on 16/12/2018
 // ---------------------------------------------------------
 
-// Challenge Lecture 12-137
-// Change the program to allow players to type full words, or phrases, then move to the
-// correct location based upon their input.
-// The player should be able to type commands such as "Go West", "run South", or just "East"
-// and the program will move to the appropriate location if there is one.  As at present, an
-// attempt to move in an invalid direction should print a message and remain in the same place.
-//
-// Single letter commands (N, W, S, E, Q) should still be available.
+// Challenge Lecture 12-139
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,21 +43,6 @@ public class Main {
         tempExit.put("S", 1);
         tempExit.put("W", 2);
         locations.put(5, new Location(5, "You are in the forest",tempExit));
-
-//        locations.get(1).addExit("W", 2);
-//        locations.get(1).addExit("E", 3);
-//        locations.get(1).addExit("S", 4);
-//        locations.get(1).addExit("N", 5);
-//
-//        locations.get(2).addExit("N", 5);
-//
-//        locations.get(3).addExit("W", 1);
-//
-//        locations.get(4).addExit("N", 1);
-//        locations.get(4).addExit("W", 2);
-//
-//        locations.get(5).addExit("S", 1);
-//        locations.get(5).addExit("W", 2);
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
